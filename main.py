234374cf0158ec85b584fb5f31191ea55ff81b68
VessyP <100419309+VessyP@users.noreply.github.com>
2022-06-12 17:19:47 +0300

    Library Project - Draft 4

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import profession
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+while True:
+    try:
+        user_role = int(input("Please enter 1 if you are a librarian and 2 if you are a vendor: "))
+        if user_role == 1:
+            break
+        elif user_role == 2:
+            break
+        else:
+            print("Invalid input. Please enter 1 for Librarian or 2 for Vendor.")
+    except:
+        print("Not a nuber. Please enter 1 for Librarian or 2 for Vendor.")
 
+user = profession.Profession.get_user_role(user_role)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while user_role == 1:
+    try:
+        choice = int(input(f'''What would you like to do, {user.name}:
+              press 1 to check how many books are issued,
+              2 to search for a specific book,
+              3 to verify a member,
+              4 to check payments,
+              5 to quit: '''))
 
+        if choice == 1:
+            profession.Librarian.check_issued_books()
+        elif choice == 2:
+            profession.Librarian.search_for_book()
+        elif choice == 3:
+            profession.Librarian.verify_a_member()
+        elif choice == 4:
+            profession.Librarian.check_payments()
+        elif choice == 5:
+            print("Thank you using our library.")
+            break
+        else:
+            print("Please provide a valid number.")
+    except:
+        print("Not a nuber. Please provide a number between 1 and 5")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while user_role == 2:
+    try:
+        choice = int(input(f'''What would you like to do, {user.name}:
+              press 1 to check how many books are issues,
+              2 to check payment details,
+              3 to quit: '''))
+        if choice == 1:
+            profession.Vendor.check_issued_books()
+        elif choice == 2:
+            profession.Vendor.payment_details()
+        elif choice == 3:
+            print("Thank you using our library.")
+            break
+        else:
+            print("Please provide a valid number.")
+    except:
+        print("Not a nuber. Please provide a number between 1 and 3")
